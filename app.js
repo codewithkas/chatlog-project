35acfd221d160f5c1f34b15bb51261bb8689ae57
codewithkas <kunwarabhashsingh9@gmail.com>
2023-07-29 19:19:31 +0530

    updated again

--- app.js
+++ app.js
@@ -9,8 +9,35 @@ const app = require("express")();
 const http = require("http").Server(app);
 
 const userRoute = require("./routes/userRoute");
+const User = require("./models/userModel");
 
 app.use("/", userRoute);
+
+const io = require("socket.io")(http);
+
+var usp = io.of("/user-namespace");
+
+usp.on("connection", async function (socket) {
+  console.log("User Connected");
+
+  var userId = socket.handshake.auth.token;
+
+  await User.findByIdAndUpdate({ _id: userId }, { $set: { is_online: "1" } });
+
+  //user broadcast online status
+  socket.broadcast.emit("getOnlineUser", { user_id: userId });
+
+  socket.on("disconnect", async function () {
+    console.log("User Disconnected");
+
+    var userId = socket.handshake.auth.token;
+
+    await User.findByIdAndUpdate({ _id: userId }, { $set: { is_online: "0" } });
+
+    //user broadcast offline status
+    socket.broadcast.emit("getOfflineUser", { user_id: userId });
+  });
+});
 
 http.listen(3000, function () {
   console.log("server is running");
